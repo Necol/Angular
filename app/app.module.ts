603f6ea9549d4d291a56e83cd89db7807f3216ba
Necol <826143652@qq.com>
2017-01-18 17:19:19 +0800

    Angular-quickstart

---
+++ app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule    //  引入外部模块内容 1FormsModule用于引用数据传送NgModul装饰器
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent //引入内部内容以供随时调用  1主模块通过my-hero-detail引用HeroDetailComponent
+                                            // 2 因为hero.ts不是模块所以不用加载这里，可以直接引用
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ app/hero-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component,Input } from '@angular/core';
+import { Hero } from './hero';
+
+
+
+@Component({
+  selector:'my-hero-detail',
+  template:`
+            <div *ngIf="hero">
+            <h2>{{hero.name}} details!</h2>
+            <div>
+                <label>id: </label>{{hero.id}}
+            </div>
+            <div>
+               <label>name: </label>
+                <input [(ngModel)]="hero.name" placeholder="name"/>
+            </div>
+            </div>`
+})
+
+export class HeroDetailComponent{
+           @Input()   //因为父元素要求被关联的属性为输入属性，input将hero设置为输出属性，这样才能在父元素当成输入属性
+            hero:Hero; //这里的Input是装饰器
+}
